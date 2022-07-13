26eee2ca4db1a6bd30f4f4ef789f46d2b507e356
Maite <mladariac2t@gmail.com>
2022-07-13 10:50:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static double ivaPriceOfTheProduct(double priceProduct){
+        return priceProduct * 1.20;
+    }
+    public static void main(String[] args) {
+       System.out.println(ivaPriceOfTheProduct(555));
+    }
+}
