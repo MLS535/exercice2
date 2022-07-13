3d2b2932e945772001debbd087fcad21fe3db684
Maite <mladariac2t@gmail.com>
2022-07-13 11:06:32 +0200

    Added array

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static double ivaPriceOfTheProduct(double priceProduct){
         return priceProduct * 1.20;
     }
     public static void main(String[] args) {
        System.out.println(ivaPriceOfTheProduct(555));
+
+        String[] nombres = new String[]{"Paco", "Maria", "Juan", "Maite"};
+
+        for (int i = 0; i < nombres.length; i++) {
+        System.out.println(nombres[i]);
+        }
     }
 }
